c1484c8dfa587118e88a57a263996bf18c1f2531
Adam Jablonka <50035910+AdamJablonka@users.noreply.github.com>
2023-05-06 13:44:34 -0400

    Update Email-Client.py (POST FINAL DEMO)

--- Email-Client.py
+++ Email-Client.py
@@ -1,6 +1,5 @@
 import socket
 import urllib.request
-import os
 
 # Set up client constants
 SMTP_PORT = 25
@@ -9,6 +8,20 @@ HOST = 'localhost'  # '15.204.245.120'
 CLIENT_DOMAIN = '348.edu'
 USERNAME = 'client_username'
 PASSWORD = 'client_password'
+
+
+# receive data function takes in a large amount of data, to not overflow buffer, waits until it finishes receiving data
+# and ends when '.' message is sent from the server.
+def receive_data(sock):
+    chunks = []
+    while True:
+        chunk = sock.recv(8192)
+        if not chunk:
+            break
+        chunks.append(chunk)
+        if b'\r\n.\r\n' in chunk:
+            break
+    return b''.join(chunks).decode()
 
 
 def get_public_ip_address():
@@ -33,19 +46,49 @@ CLIENT_IP = get_public_ipv4()
 
 
 # Define SMTP functions
-def smtp_client(sender, receiver, subject, message):
+def smtp_client():
+
     # Connect to SMTP server socket
     smtp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     smtp_client_socket.connect((HOST, SMTP_PORT))
 
     # Receive SMTP greeting from server
     data = smtp_client_socket.recv(1024)
-    print(data.decode())
 
     # Send EHLO command to start SMTP handshake
     smtp_client_socket.send('HELO {}\r\n'.format(CLIENT_DOMAIN).encode())
     data = smtp_client_socket.recv(1024)
+    print(data.decode(), end='')
+
+    print("LOGIN: ")
+    auth_login_input = input(
+        'Enter in this format (USERNAME PASSWORD): ')
+    # password = input('Enter Password: ')
+    try:
+        sender = auth_login_input.split()[0]
+    except:
+        print("ERROR wrong input")
+        return False
+
+    try:
+        password = auth_login_input.split()[1]
+    except:
+        print("ERROR wrong input")
+        return False
+
+    # Send AUTH LOGIN command to start SMTP handshake
+    smtp_client_socket.send(
+        'AUTHLOGIN {}\r\n'.format(auth_login_input).encode())
+    data = smtp_client_socket.recv(1024)
     print(data.decode())
+    if data == b'535\r\n':
+        print('535 Authentication credentials invalid')
+        print('Please enter a correct username or password and try again.')
+        return False
+
+    receiver = input('Enter recipient: ')
+    subject = input('Enter email subject: ')
+    message = input('Enter email body: ')
 
     # Send MAIL FROM command to SMTP Server
     smtp_client_socket.send('MAIL FROM: <{}>\r\n'.format(sender).encode())
@@ -72,7 +115,7 @@ def smtp_client(sender, receiver, subject, message):
     print(data.decode())
 
     # Send QUIT command
-    smtp_client_socket.send(b'QUIT\r\n');
+    smtp_client_socket.send(b'QUIT\r\n')
     data = smtp_client_socket.recv(1024)
     print(data.decode())
 
@@ -84,163 +127,91 @@ def smtp_client(sender, receiver, subject, message):
 # Define POP3 functions
 def pop3_client():
     # Connect to POP3 server socket
+    print("Connecting to POP3 Server...")
     pop3_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    pop3_client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
     pop3_client_socket.connect((HOST, POP3_PORT))
 
     # Receive POP3 greeting from server
     data = pop3_client_socket.recv(1024)
     print(data.decode(), end='')
+    print("LOGIN: ")
+    USERNAME = input("Please enter your username: ")
+    PASSWORD = input("Please enter your password: ")
+
+    if len(USERNAME) == 0 or len(PASSWORD) == 0:
+        print("ERROR wrong input detected")
+        return False
 
     # Send USER command to start POP3 handshake
     pop3_client_socket.send('USER {}\r\n'.format(USERNAME).encode())
     data = pop3_client_socket.recv(1024)
+
     print(data.decode(), end='')
+    if data == b'-ERR User not found\r\n':
+        print('Please enter a correct username or password and try again.')
+        return False
 
     # Send PASS command to continue POP3 handshake
     pop3_client_socket.send('PASS {}\r\n'.format(PASSWORD).encode())
     data = pop3_client_socket.recv(1024)
     print(data.decode(), end='')
 
-    # send LIST command to list messages
-    pop3_client_socket.send(b"LIST\r\n")
-    data = pop3_client_socket.recv(1024)
-    print(data.decode())
+    while True:
+        print("Menu:")
+        print("1. LIST Command")
+        print("2. RETR Command")
+        print("3. DELE command")
+        print("4. QUIT Command")
 
-    # send QUIT command to log out and close the connection
-    pop3_client_socket.send(b"QUIT\r\n")
-    data = pop3_client_socket.recv(1024)
-    print(data.decode())
+        choice = input("Enter your choice (1-4): ")
+        print("-----------------------")
 
-    # Close POP3 connection
-    pop3_client_socket.close()
-    print('client closed')
+        if choice == '1':
+            # send LIST command to list messages
+            pop3_client_socket.send(b"LIST\r\n")
+            data = receive_data(pop3_client_socket)
+            print(data)
+        elif choice == '2':
+            # send RETR command to retrieve a message
+            message_number = input("Enter the message number to retrieve: ")
+            pop3_client_socket.send(f"RETR {message_number}\r\n".encode())
+            # Increase the buffer size for larger emails
+            data = receive_data(pop3_client_socket)
+            print(data)
+            # data = pop3_client_socket.recv(1024)
+            # print(data.decode())
+        elif choice == '3':
+            message_number = input("Enter the message number to delete: ")
+            print('Sending DELE command...')
+            pop3_client_socket.send(f"DELE {message_number}\r\n".encode())
+            response = pop3_client_socket.recv(1024)
+            print(f'DELE response: {response.decode()}')
+        elif choice == '4':
+            # send QUIT command to log out and close the connection
+            pop3_client_socket.send(b"QUIT\r\n")
+            data = pop3_client_socket.recv(1024)
+            print(data.decode())
 
-"""
-def user_exists(user_input):
-    if os.path.isdir(user_input):
-        return True
-    else:
-        return False
-
-
-def register_user(user_input, password_input):
-    # make user directory
-    os.mkdir(user_input)
-
-    # make credentials file
-    with open(user_input + '/credentials.txt', 'w') as file:
-        file.write('username:' + user_input + '\n')
-        file.write('password:' + password_input + '\n')
-
-    # make mail folder
-    os.mkdir(user_input + '/mail')
-
-    return True
-
-
-def run_authenticate(user_input, password_input):
-    credentials_file = user_input + '/credentials.txt'
-    print(f"credentials_file: {credentials_file}")
-    print('running authenticate!')
-
-    # check if the cred file exists
-    if not os.path.isfile(credentials_file):
-        print('file doesnt exist!')
-        return False
-
-    print('file exists')
-    # read stored creds from file
-    with open(credentials_file, 'r') as file:
-        print('opening to read creds!')
-        stored_username = file.readline().strip().split(':')[1]
-        print(f'stored user: {stored_username}')
-        stored_pass = file.readline().strip().split(':')[1]
-        print(f'stored pass: {stored_pass}')
-
-    # compare stored cred to input
-    if user_input == stored_username and password_input == stored_pass:
-        return True
-    else:
-        return False
+            # Close POP3 connection
+            pop3_client_socket.close()
+            print('client closed')
+            break
+        else:
+            # Invalid input
+            print("Invalid choice. Please try again.")
 
 
-authenticated = False
-register = False
-while True:
-    if not authenticated or not register:
-        start_choice = input('Register or Login (R/L): ')
-
-    if start_choice.lower() == 'r':
-        register = True
-
-    if register:
-        while True:
-            username = input('Enter Desired User Name: ')
-            if user_exists(username):
-                print('User already exists. Please choose another username.')
-            else:
-                password = input('Enter Desired Password:')
-                register_user(username, password)
-                register = False
-                break
-
-    if start_choice == 'l' and not authenticated:
-        choice = input("Enter User:")
-        USERNAME = choice
-        choice = input("Enter Password:")
-        PASSWORD = choice
-        # authenticate METHOD here TODO
-        while True:
-            if run_authenticate(USERNAME, PASSWORD):
-                break
-            else:
-                print('Wrong info!')
-                choice = input("Enter User:")
-                USERNAME = choice
-                choice = input("Enter Password:")
-                PASSWORD = choice
-        authenticated = True
-        print(f"Welcome {USERNAME}")
-
-    if authenticated:
-        choice = input("Enter 'send' or 'mail':")
-        if choice == 'send':
-            sender = USERNAME
-            receiver = input('To user: ')
-            subject = input('Enter email subject: ')
-            message = input('Enter email message: ')
-            while True:
-                if not user_exists(username):
-                    print('sent.')
-                    break
-                else:
-                    print('sent')
-                    break
-            smtp_client('test@348.edu', 'yo yo', 'this is a test message!')
-        if choice == 'mail':
-            print('implement pop here')
-"""
-
-user_inputted = False
 done = False
 while not done:
-    if not user_inputted:
-        username_input = input('Enter Username: ')
-        if '@' not in username_input:
-            print("please enter a valid email!")
-            continue
-        print(f'Hello {username_input}!')
-
-    user_inputted = True
-    y_n = input('Do you wish to send (Y/N): ')
-    if y_n.lower() == 'y':
-        receiver = input('Enter receiver: ')
-        if '@' not in receiver:
-            print("please enter a valid email for receiver!")
-            continue
-        subject = input('Enter subject: ')
-        message = input('Enter message: ')
-        smtp_client(username_input, receiver, subject, message)
+    s_r = input('Send(s) or Read email(r) or Quit(any input): ')
+    if s_r.lower() == 's':
+        smtp_client()
+        done = True
+    elif s_r.lower() == 'r':
+        pop3_client()
+        done = True
+        break
     else:
         print('Bye!')
         done = True
